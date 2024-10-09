4fdcfa16d6906df8ea7547da203ebda9a07b4d90
oopsmails <oopsmails@gmail.com>
2024-10-09 11:49:10 -0400

    separate modules, signal init

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { CommonModule, DecimalPipe, registerLocaleData } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import localeFr from '@angular/common/locales/fr';
+import { AppRoutingModule } from './app.routes';
+import { HomeModule } from './pages/home/home.module';
+// registerLocaleData(localeDe);
+registerLocaleData(localeFr);
+// registerLocaleData(localeEn);
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    HomeModule,
+    AppRoutingModule, // need to be at last, otherwise NotFoundComponent is at front
+  ],
+  declarations: [],
+  exports: [],
+  providers: [
+    DecimalPipe,
+  ],
+  bootstrap: [],
+})
+export class AppModule { }
--- src/app/shared/models/user.ts
+++ src/app/shared/models/user.ts
@@ -1,6 +1,14 @@
 export interface User {
   id: string;
-  name: string;
-  age: number;
-  email: string;
+  name?: string;
+  age?: number;
+  email?: string;
+  lastName?: string;
+  username?: string;
+}
+
+export interface ModifiedUser {
+  id: string;
+  displayName?: string;
+  username?: string;
 }
