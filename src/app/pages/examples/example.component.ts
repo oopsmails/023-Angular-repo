bb0aa1eab8e62cce6f6b0a04e491f4369dcd8311
oopsmails <oopsmails@gmail.com>
2024-10-11 11:36:25 -0400

    multiple modules

--- src/app/pages/examples/example.component.ts
+++ src/app/pages/examples/example.component.ts
@@ -1,7 +1,6 @@
 import { AsyncPipe, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { User } from '../../shared/models/user';
 import { UserListComponent } from '../home/signal/user-list/user-list.component';
 
 @Component({
@@ -12,25 +11,6 @@ import { UserListComponent } from '../home/signal/user-list/user-list.component'
   styleUrl: './example.component.scss'
 })
 export class ExampleComponent {
-  users: User[] = [
-    { id: "1", name: 'Michael', lastName: 'Scott', username: 'michael.scott' },
-    { id: "2", name: 'Dwight', lastName: 'Schrute', username: 'dwight.schrute' },
-    { id: "3", name: 'Angela', lastName: 'Martin', username: 'angela.martin' },
-    { id: "4", name: 'Jim', lastName: 'Halpert', username: 'jim.halpert' },
-  ];
-
-  addUser() {
-    this.users = [
-      {
-        id: "5",
-        name: 'Andy',
-        lastName: 'Bernard',
-        username: 'andy.bernard',
-      },
-      ...this.users,
-    ];
-    console.log('Current Users', this.users);
-  }
 
 }
 
